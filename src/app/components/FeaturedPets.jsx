2ad0c4e65910d501df1e161c362de10a0bd58ec3
Mira-2234 <tasmimmira822@gmail.com>
2026-05-28 21:27:47 +0600

    change url

--- src/app/components/FeaturedPets.jsx
+++ src/app/components/FeaturedPets.jsx
@@ -3,21 +3,20 @@ import PetCard from './PetCard'
 
 export default async function FeaturedPets() {
 
-   const res = await fetch(`https://assignment-9-server-1-cxv0.onrender.com/pets`, {
-    cache: "no-store"
-});
+    let pets = [];
 
-if (!res.ok) {
-    console.error("Fetch failed:", res.status);
-    return <div>Failed to load pets</div>;
-}
+    try {
+        const res = await fetch(`https://assignment-9-server-1-cxv0.onrender.com/pets`, {
+            cache: "no-store"
+        });
 
-const data = await res.json();
-const pets = Array.isArray(data) ? data : [];
-
-    const pets = await res.json();
-
-    console.log(pets);
+        if (res.ok) {
+            const data = await res.json();
+            pets = Array.isArray(data) ? data : [];
+        }
+    } catch (err) {
+        console.error("Fetch failed:", err.message);
+    }
 
     return (
         <div className='py-10'>
@@ -26,28 +25,18 @@ const pets = Array.isArray(data) ? data : [];
                 <span className="text-primary-600 text-xl font-semibold text-gray-400">
                     Ready for Adoption
                 </span>
-
                 <h2 className="section-title mt-2 text-6xl text-green-500 font-bold mb-3">
                     Featured Pets
                 </h2>
-
                 <p className="text-gray-500 max-w-xl mx-auto text-sm">
                     Meet adorable friends looking for their forever home.
                 </p>
             </div>
 
             <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-
-                {
-                    Array.isArray(pets) &&
-                    pets.slice(0, 6).map((pet, ind) => (
-                        <PetCard
-                            pet={pet}
-                            key={pet._id || ind}
-                        />
-                    ))
-                }
-
+                {pets.slice(0, 6).map((pet, ind) => (
+                    <PetCard pet={pet} key={pet._id || ind} />
+                ))}
             </div>
 
         </div>
